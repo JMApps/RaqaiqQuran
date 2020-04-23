bcbe7a4848bd814cbaad6b1a344e9124a7c85293
dev_muslim <jmappsmuslim@gmail.com>
2020-04-23 15:10:42 +0300

    Fab button hide/show

--- app/src/main/kotlin/jmapps/raqaiqquran/MainActivity.kt
+++ app/src/main/kotlin/jmapps/raqaiqquran/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.appbar.AppBarLayout
 import jmapps.raqaiqquran.databinding.ActivityMainBinding
 import jmapps.raqaiqquran.ui.main.SectionsPagerAdapter
 
@@ -20,9 +21,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.diMainContainer.attachViewPager(binding.vpMainContainer)
         binding.diMainContainer.setDotTintRes(R.color.red)
 
+        binding.appBarMain.addOnOffsetChangedListener(offsetChangedListener)
         binding.fabChapters.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
+    }
+
+    private val offsetChangedListener = AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
+        if (verticalOffset < 0) {
+            binding.fabChapters.hide()
+        } else {
+            binding.fabChapters.show()
+        }
     }
 }
