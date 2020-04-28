5537ede9b40e03249abab02dcc7384201b2d5d03
dev_muslim <jmappsmuslim@gmail.com>
2020-04-28 12:10:28 +0300

    Added about us dialog

---
+++ app/src/main/kotlin/jmapps/raqaiqquran/ui/aboutus/AboutUsBottomSheet.kt
@@ -0,0 +1,24 @@
+package jmapps.raqaiqquran.ui.aboutus
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import jmapps.raqaiqquran.R
+
+class AboutUsBottomSheet : BottomSheetDialogFragment() {
+
+    private lateinit var rootAboutUs: View
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        rootAboutUs = inflater.inflate(R.layout.bottom_sheet_about_us, container, false)
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        return rootAboutUs
+    }
+
+    companion object {
+        const val aboutUsTag = "AboutUsBottomSheet"
+    }
+}
